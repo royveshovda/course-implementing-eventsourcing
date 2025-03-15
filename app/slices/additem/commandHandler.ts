0f9d212da173dcd7de80e794880755c9c40d8dff
Martin Dilger <martin@nebulit.de>
2025-03-15 14:47:54 +0100

    solution

--- app/slices/additem/commandHandler.ts
+++ app/slices/additem/commandHandler.ts
@@ -5,12 +5,33 @@ import {findEventStore} from "@/app/infrastructure/inmemoryEventstore";
 
 export const addItemCommandHandler = async (events: CartEvents[], command: AddItemCommand): Promise<CartEvents[]> => {
 
-    return [
-        /*{
+    var itemsInCart = events.reduce((acc: string[], event: CartEvents) => {
+       if (event.type === 'ItemAdded') {
+            acc.push(event.data.itemId);
+        } else if (event.type === 'ItemRemoved') {
+            acc = acc.filter(itemId => itemId !== event.data.itemId);
+        } else if(event.type === 'ItemArchived'){
+            acc = acc.filter(itemId => itemId !== event.data.itemId);
+        } else if (event.type == "CartCleared") {
+           acc = []
+       }
+        return acc; // Return the updated accumulator
+    }, []);
+
+    if (itemsInCart.length >= 3) {
+        throw new Error("You can only have 3 items in your cart");
+    }
+
+    return [{
         type: 'ItemAdded',
         data: {
-             TODO apply items
+            aggregateId: command.data.name,
+            description: command.data.description,
+            itemId: command.data.itemId,
+            name: command.data.name,
+            productId: command.data.productId,
+            price: command.data.price
         }
-    }*/]
+    }]
 
 }
